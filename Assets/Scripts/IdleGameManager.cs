2aadfbc7ad2508d56d2b65446d8c45f60e17a520
dkNykita <domca.herak@gmail.com>
2022-03-30 19:32:21 +0200

    episode 2

--- Assets/Scripts/IdleGameManager.cs
+++ Assets/Scripts/IdleGameManager.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+
+// Time.deltaTime - Fix update on different framerates
+// .ToString("F0") - convert value to string with FX decimals
+
 public class IdleGameManager : MonoBehaviour
 {
     
@@ -8,6 +12,8 @@ public class IdleGameManager : MonoBehaviour
     public Text coinsText;
     // Owned coins value
     public double coins;
+    // How much you get per click on game start
+    public double coinsClickValueInitial;
     // How much you get per click
     public double coinsClickValue;
     // Text for CPS
@@ -34,6 +40,8 @@ public class IdleGameManager : MonoBehaviour
     public double productionUpgrade1Generation;
     // Click Upgrade 1 total coin generation
     public double clickUpgrade1Generation;
+    // Text for Click Button
+    public Text clickText;
 
     
     
@@ -43,10 +51,12 @@ public class IdleGameManager : MonoBehaviour
     public void Start()
     {
         coins = 10;
-        coinsClickValue = 1;
+        coinsClickValueInitial = 1;
         clickUpgrade1Cost = 10;
+        clickUpgrade1Level = 0;
         clickUpgrade1Power = 1;
         productionUpgrade1Cost = 25;
+        productionUpgrade1Level = 0;
         productionUpgrade1Power = 1;
     }
 
@@ -54,20 +64,29 @@ public class IdleGameManager : MonoBehaviour
     public void Update()
     {
         coinsPerSecond = productionUpgrade1Generation;
-        clickUpgrade1Generation = clickUpgrade1Level * clickUpgrade1Power;
-        coinsClickValue = (clickUpgrade1Generation);
+        coinsClickValue = coinsClickValueInitial + clickUpgrade1Generation;
+        clickText.text = "Click\n+" + coinsClickValue + " Coins";
         coinsText.text = "Coins: " + coins.ToString("F0");
         coinsPerSecondText.text = coinsPerSecond.ToString("F0") + " Coins /s";
         upgradeClick1Text.text = "Click Upgrade 1\n" + "Cost: " + clickUpgrade1Cost.ToString("F0") + " Coins\n" + "Power: +" + clickUpgrade1Power.ToString("F0") + " Click\n" + "Level: " + clickUpgrade1Level.ToString("F0");
         upgradeProduction1Text.text = "Production Upgrade 1\n" + "Cost: " + productionUpgrade1Cost.ToString("F0") + " Coins\n" + "Power: +" + productionUpgrade1Power.ToString("F0") + "Coins/s\n" + "Level: " + productionUpgrade1Level.ToString("F0");
         coins += (coinsPerSecond * Time.deltaTime);
+        if(coins < 0)
+            {
+                coins = 0;
+                coinsText.text = "Error: Negative Coins";
+            }
+        if(coinsPerSecond < 0)
+            {
+                coinsPerSecondText.text = "Error: Negative Coins /s";
+            }        
 
     }
 
     // Buttons
     public void Click()
     {
-        coins += coinsClickValue+clickUpgrade1Generation;
+        coins += coinsClickValue;
     }
 
     public void BuyClickUpgrade1()
@@ -76,7 +95,8 @@ public class IdleGameManager : MonoBehaviour
         {
             coins -= clickUpgrade1Cost;
             clickUpgrade1Level += 1;
-            clickUpgrade1Cost *= 1.07;            
+            clickUpgrade1Cost *= 1.07;             
+            clickUpgrade1Generation = clickUpgrade1Level * clickUpgrade1Power;          
         }
     }
 
